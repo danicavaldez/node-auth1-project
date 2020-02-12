87c2ff8ee1a9dcdc54488e157e227275325a6a95
Danica Valdez <danicaevaldez@gmail.com>
2020-02-11 22:39:47 -0900

    auth middleware created

---
+++ auth/auth-required-middleware.js
@@ -0,0 +1,24 @@
+const users = require("../users/users-model");
+const bcrypt = require("bcryptjs");
+
+module.exports = (req, res, next) => {
+  const { username, password } = req.headers;
+
+  if (!(username && password)) {
+    res.status(401).json({ message: "Invalid Credentials" });
+  } else {
+    users
+      .findBy({ username })
+      .first()
+      .then(_user => {
+        if (_user && bcrypt.compareSync(password, _user.password)) {
+          next();
+        } else {
+          res.status(401).json({ message: "invalid credentials" });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ message: err });
+      });
+  }
+};
